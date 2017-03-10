ff3a2a34bd0ca096a30288cdeab46635640cb022
Hany Heikal <Hany.Heikal@gmail.com>
2017-03-10 13:10:41 -0600

    check voucher length

--- index.js
+++ index.js
@@ -12,7 +12,22 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
-    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+   // var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    var speech = "";
+    if (req.body.result && req.body.result.parameters) {
+        if (req.body.result.parameters.echoText) {
+            speech = req.body.result.parameters.echoText;
+        } else if (req.body.result.parameters.voucherPin) {
+            var voucher = req.body.result.parameters.voucherPin;
+            if (voucher.length == '14') {
+                speech = "This is a valid voucher";
+            } else {
+                speech = This is not a valid voucher";
+            }
+        }
+    } else {
+        speech = "Seems like some problem. Speak again.";
+    }
     return res.json({
         speech: speech,
         displayText: speech,
